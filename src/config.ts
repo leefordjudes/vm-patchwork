23f38eaad2b7abd20b1cf9c959d015b08ec7e0b1
esakkimuthu-p <esakkimuthu556@gmail.com>
2021-01-25 18:21:21 +0530

    ulr updated

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const URI = `mongodb://localhost/sample`;
+// export const URI = `mongodb://localhost/sample`;
+export const URI = `mongodb+srv://testadmin:rootroot@auditplus-test.dqqxs.mongodb.net/velavanmedical?retryWrites=true&w=majority`
