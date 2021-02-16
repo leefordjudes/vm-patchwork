181b0570383376a7a782be617b7ba9cfdd9cfee3
leefordjudes <leefordjudes@gmail.com>
2021-02-17 05:05:12 +0530

    updated

--- src/inv-voucher.service.ts
+++ src/inv-voucher.service.ts
@@ -19,6 +19,7 @@ export class Patch9Service {
       return err;
     }
     try {
+      /*      
       const connectdb = connection.db().databaseName;
       console.log(connectdb);
       const auditplusDB = await connection
@@ -272,7 +273,7 @@ export class Patch9Service {
         .db()
         .collection('batches')
         .updateMany({}, { $unset: { closing: true, sold: true } });
-      /*
+      //*
       const limit = 25000;
       const saleCount = await connection.db().collection('sales').countDocuments();
       if (saleCount > 0) {
