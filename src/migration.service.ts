0f666022030ed6fd461d5b8785c0c7d0ddb07984
esakkimuthu-p <esakkimuthu556@gmail.com>
2021-07-15 13:50:29 +0530

    org clone added

--- src/migration.service.ts
+++ src/migration.service.ts
@@ -7,6 +7,7 @@ import { URI } from './config';
 import { GST_TAXES } from './fixtures/gst-tax';
 import { STATE } from './fixtures/state/state';
 import { round } from './utils/utils';
+import { VOUCHER_TYPE } from './fixtures/voucher-type/voucher-type';
 
 @Injectable()
 export class MigrationService {
@@ -19,6 +20,161 @@ export class MigrationService {
     if (!connection.isConnected) {
       return 'Connection Failed';
     }
+    console.log('velavanmed org clone started...');
+    const newOrg = 'velavanmed';
+    const oldOrg = 'velavanmedical';
+    const date = new Date();
+    const branch = Types.ObjectId('5f414f9f6daef187462bbe03');
+    const user = (await connection.db(newOrg).collection('users').findOne({ isAdmin: true }))._id;
+    const invHead = await connection.db(oldOrg).collection('inventory_heads').findOne({ name: 'Default' });
+    await connection.db(newOrg).collection('inventory_heads').drop();
+    await connection.db(newOrg).collection('inventory_heads').insertOne(invHead);
+
+    const oldBranch = await connection.db(oldOrg).collection('branches').findOne({ _id: branch });
+    oldBranch.desktopClients = [];
+    oldBranch.users = [user];
+    await connection.db(newOrg).collection('branches').insertOne(oldBranch);
+
+    const newBranchAccDoc = {
+      _id: oldBranch.account,
+      name: oldBranch.displayName,
+      displayName: oldBranch.displayName,
+      validateName: oldBranch.validateName,
+      accountType: 'BRANCH_TRANSFER',
+      hide: false,
+      createdBy: user,
+      updatedBy: user,
+      createdAt: date,
+      updatedAt: date,
+    };
+    await connection.db(newOrg).collection('accounts').insertOne(newBranchAccDoc);
+
+    const invClonePipe = [
+      {
+        $addFields: {
+          createdBy: user,
+          updatedBy: user,
+          createdAt: date,
+          updatedAt: date,
+        }
+      },
+      {
+        $project: {
+          vendorId: 0,
+          vendorName: 0,
+        }
+      },
+      {
+        $merge: { into: { db: newOrg, coll: 'inventories' } }
+      },
+    ];
+    await connection.db(oldOrg).collection('inventories').aggregate(invClonePipe).toArray();
+
+    const rackClonePipe = [
+      {
+        $addFields: {
+          createdBy: user,
+          updatedBy: user,
+          createdAt: date,
+          updatedAt: date,
+        }
+      },
+      {
+        $merge: { into: { db: newOrg, coll: 'racks' } }
+      },
+    ];
+    await connection.db(oldOrg).collection('racks').aggregate(rackClonePipe).toArray();
+
+    const saltClonePipe = [
+      {
+        $addFields: {
+          createdBy: user,
+          updatedBy: user,
+          createdAt: date,
+          updatedAt: date,
+        }
+      },
+      {
+        $merge: { into: { db: newOrg, coll: 'pharma_salts' } }
+      },
+    ];
+    await connection.db(oldOrg).collection('pharma_salts').aggregate(saltClonePipe).toArray();
+
+    const sectionClonePipe = [
+      {
+        $addFields: {
+          createdBy: user,
+          updatedBy: user,
+          createdAt: date,
+          updatedAt: date,
+        }
+      },
+      {
+        $merge: { into: { db: newOrg, coll: 'sections' } }
+      },
+    ];
+    await connection.db(oldOrg).collection('sections').aggregate(sectionClonePipe).toArray();
+
+    const manufacturerClonePipe = [
+      {
+        $addFields: {
+          createdBy: user,
+          updatedBy: user,
+          createdAt: date,
+          updatedAt: date,
+        }
+      },
+      {
+        $merge: { into: { db: newOrg, coll: 'manufacturers' } }
+      },
+    ];
+    await connection.db(oldOrg).collection('manufacturers').aggregate(manufacturerClonePipe).toArray();
+
+    const unitClonePipe = [
+      {
+        $addFields: {
+          createdBy: user,
+          updatedBy: user,
+          createdAt: date,
+          updatedAt: date,
+        }
+      },
+      {
+        $merge: { into: { db: newOrg, coll: 'units' } }
+      },
+    ];
+    await connection.db(oldOrg).collection('units').aggregate(unitClonePipe).toArray();
+
+    const invBranchDetailClonePipe = [
+      {
+        $match: {
+          branch
+        }
+      },
+      {
+        $merge: { into: { db: newOrg, coll: 'inv_branch_details' } }
+      },
+    ];
+    await connection.db(oldOrg).collection('inv_branch_details').aggregate(invBranchDetailClonePipe).toArray();
+
+    const fYear = (await connection.db(newOrg).collection('financial_years').findOne({}))._id;
+    const voucherNumberingArr = VOUCHER_TYPE.map((elm) => {
+      return {
+        voucherType: elm.defaultName,
+        branch,
+        sequence: 1,
+        prefix: 'TG',
+        suffix: '',
+        fYear,
+        createdAt: date,
+        updatedAt: date,
+      }
+    });
+    await connection.db(newOrg).collection('voucher_numberings').insertMany(voucherNumberingArr);
+
+    console.log('velavanmed org clone END...');
+
+    console.log('Round off started...');
     const dbs = ['velavanstationery', 'velavanhm', 'ttgold', 'ttgoldpalace', 'auditplustech', 'ramasamy', 'velavanmedical'];
     const results = [];
     const saleCollection = 'sales';
@@ -27,6 +183,7 @@ export class MigrationService {
       const sales = await connection.db(db).collection(saleCollection).updateMany(
         {
           date: { $gte: new Date('2020-04-01T00:00:00.000+0000'), $lte: new Date('2021-06-29T00:00:00.000+0000') },
+          __v: { $exists: false },
           voucherType: 'SALE',
           'acAdjs.roundedOff': { $exists: true },
         },
@@ -39,6 +196,7 @@ export class MigrationService {
         {
           date: { $gte: new Date('2020-04-01T00:00:00.000+0000'), $lte: new Date('2021-06-29T00:00:00.000+0000') },
           voucherType: 'DEBIT_NOTE',
+          __v: { $exists: false },
           'acAdjs.roundedOff': { $exists: true },
         },
         { $mul: { 'acAdjs.roundedOff': -1 } },
@@ -46,6 +204,7 @@ export class MigrationService {
 
       results.push({ orgName: db, collectionName: purchaseCollection, matchedCount: purchases.matchedCount, modifiedCount: purchases.modifiedCount });
     }
+    console.log('Round off end...');
     console.log(results);
     return results;
   }
